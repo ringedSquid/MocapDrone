601c6a971207bf08dd1bdba9799b56fe4957e939
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2024-10-18 23:41:16 -0400

    ok, don't send entire images through the middlewear

--- src/send_image.cpp
+++ src/send_image.cpp
@@ -12,13 +12,23 @@ using namespace lcm;
 using namespace image_types;
 
 int main(int argc, char **argv) {
-	std::string image_path = samples::findFile("catTest.jpg");
-	Mat img = imread(image_path, IMREAD_GRAYSCALE);
-
-	image_grey_t img;
-
-
 	LCM lcm;
 	if (!lcm.good()) {
 		return 1;
 	}
+
+	std::string image_path = samples::findFile("catTest.jpg");
+	Mat img_open = imread(image_path, IMREAD_GRAYSCALE);
+
+	image_grey_t img;
+
+	img.rows = img_open.rows;
+	img.cols = img_open.cols;
+	img.size = img.rows * img.cols;
+	//printf("r: %d, c: %d, s: %d\n", img.rows, img.cols, img.size);
+	img.data.resize(img.size);
+	std::memcpy(img.data.data(), img_open.data, img.size*sizeof(uint8_t));
+
+	lcm.publish("IMAGE", &img);
+	return 0;
+}
