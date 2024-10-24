a45fe063b7e4dc9c1c46086733a325d502dd36b6
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2024-10-24 00:10:20 -0400

    remove build dir

--- build/CMakeFiles/type_definitions-cpp.sources.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/type_definitions-cpp.sources"
-  "project_types/image_points_t.hpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/type_definitions-cpp.sources.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
